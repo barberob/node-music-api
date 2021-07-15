372ace0dc7a19985f91ff0beb9000a52abeb96a9
michelgege <50527053+michelgege@users.noreply.github.com>
2021-07-15 13:53:51 +0200

    Revert "search empty error"
    This reverts commit 2330b9f7c880e44e9dac0b7b1c851752988fe163.

--- routes/songs.mjs
+++ routes/songs.mjs
@@ -25,10 +25,6 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/search/:searchValue', async (req, res) => {
-    if (!req.params.searchValue) {
-        res.json({ error: true, message: 'No search parameter specified' })
-        return
-    }
     try {
         const songs = await SongModel.aggregate([
             {
